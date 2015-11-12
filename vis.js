6bce66df5c96574887070f92a1725d49c0c2b39e
radiocontrolled <radiocontrolled@gmail.com>
2015-11-12 20:11:40 +0000

    moving the slider rail increases the circle size

--- vis.js
+++ vis.js
@@ -1,64 +1,89 @@
 (function(){
 
-  var data,
+  var data = "foo", 
     circle, 
     svg, 
     arr = [], 
     years = [], 
-    processedData = [];
+    processedData = [], 
+    newData;
 
   d3.csv("pop.csv", function(error, json) {
-    if (error) console.log("error");
-    else data = json;  
+    if (error) {
+      console.log("error");
+    }
+    else {
+      data = json;  
+    }
     init();
   });
 
-  function init() {
+  function drawCircle() {
 
+    svg = d3.select("#vis")
+      .append("svg")
+      .attr({
+        "width": function() {
+         return $(window).width() * 0.5; 
+      },
+        "height": function() {
+        return $(window).height() * 0.5;
+      }
+    });
+
+    var temp = [];
+    temp.push(processedData[0][1]);
+
+    circle = svg.selectAll("circle")
+      .data(temp)
+      .enter()
+      .append("circle")
+      .attr({
+        "cx" : 100, // fix later
+        "cy" : 100, 
+        "r" : function(d, i) {
+          return temp / 1000000;
+        }, 
+        "fill" : "#efefef"
+      }
+    );
+
+    initialiseSlider(processedData[0][0],processedData[processedData.length - 1][0]);
+  
+  }
+
+  function init() {
     for(var k = 0; k < data.length; k++ ) {
       var inner = [];
       inner.push(parseInt(data[k].Year));
       inner.push(parseInt(data[k].UK));
       processedData.push(inner);
     }
-
-    // processed data - to build into a function 
-    // console.log(arr);
-    // console.log(processedData[0][1]);
-
-    svg = d3.selectAll("#vis")
-      .append("svg")
-      .attr({
-        "width": function() {
-          return $(window).width() * 0.5; 
-        },
-        "height": function() {
-          return $(window).height() * 0.5;
-        }
-      });
-
-    arr.push(data[0].UK);
-
-    for(var i = 0; i < data.length; i++) {
-      years.push(data[i].Year);
-    }
-
-    circle = svg.selectAll("#vis circle")
-      .data(arr)
-      .enter()
-      .append("circle")
-      .attr({
-        "cx" : 100, // fix later
-        "cy" : 100, 
-        "r" : function(d) {
-          return d / 1000000;
-        }, 
-        "fill" : "#efefef"
-      });
-
-    initialiseSlider(processedData[0][0],processedData[processedData.length - 1][0]);
+    
+    drawCircle();
   }
 
+
+  function update(year) {
+
+    var temp = [];
+
+    for(var i = 0; i < processedData.length; i++) {
+      if(processedData[i][0] == year) {
+        temp.push(processedData[i][1]);
+      }
+    }
+
+    circle = svg.selectAll("circle")
+      .data(temp)
+      .attr({
+        "r" : function(d, i) {
+          return temp / 1000000;
+        }
+      }
+    );
+
+  }
 
   function initialiseSlider(start, end) {
     $("#slider").slider({
@@ -68,7 +93,7 @@
       step: 1,
       slide: function(event, ui) {
         $("#year").val(ui.value);
-        console.log(ui.value);
+        update(ui.value);
       }
     });
   }
